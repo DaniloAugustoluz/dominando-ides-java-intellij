cba72bbb3fa7648aec674916420aca0aa609d76b
Danilo Augusto Souza Luz <89174886+DaniloAugustoluz@users.noreply.github.com>
2021-09-11 02:02:31 -0300

    First commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+         /*
+        int a = 10;
+        int b = 15;
+        int c = a + b;
+        System.out.println("Hello World! " + c);
+        */
+    }
+}

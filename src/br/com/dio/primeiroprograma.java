cd20c540e0215b4e098afdcbfe20e7ce7d3cda58
Danilo Augusto Souza Luz <89174886+DaniloAugustoluz@users.noreply.github.com>
2021-09-11 02:15:45 -0300

    Criando Classe Livro

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -5,8 +5,10 @@ import br.com.dio.model.Gato;
 public class primeiroprograma {
     public static void main(String[] args) {
 
+        Livro livro = new Livro("O Probelma do Danilo", 300);
         Gato gato = new Gato();
         System.out.println(gato);
+        System.out.println(livro);
 
          /*
         int a = 10;
@@ -14,5 +16,38 @@ public class primeiroprograma {
         int c = a + b;
         System.out.println("Hello World! " + c);
         */
+    }
+}
+class Livro{
+    private String nome;
+    private Integer numpag;
+
+    public Livro(String nome, Integer numpag) {
+        this.nome = nome;
+        this.numpag = numpag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpag() {
+        return numpag;
+    }
+
+    public void setNumpag(Integer numpag) {
+        this.numpag = numpag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpag=" + numpag +
+                '}';
     }
 }
